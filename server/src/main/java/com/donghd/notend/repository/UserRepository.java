dbe30c0966b193bc71a82a7cee3cf7d27e6f29a4
Ho Duc Dong <dong.hoduc@nccsoft.vn>
2018-09-03 19:11:53 +0700

    add paid user function

--- server/src/main/java/com/donghd/notend/repository/UserRepository.java
+++ server/src/main/java/com/donghd/notend/repository/UserRepository.java
@@ -20,6 +20,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findOneByActivationKey(String activationKey);
 
     List<User> findAllByActivatedIsFalseAndCreatedDateBefore(Instant dateTime);
+    
+    List<User> findAllByPaidUserIsTrueAndExpirationDateBefore(Instant dateTime);
 
     Optional<User> findOneByResetKey(String resetKey);
 
