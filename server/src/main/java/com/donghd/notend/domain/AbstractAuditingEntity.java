1f38e7c867aaf0e3f94ef8ff17bf15924ba60716
Ho Duc Dong <dong.hoduc@nccsoft.vn>
2018-09-05 23:47:52 +0700

    #Fix bug null langKey

--- server/src/main/java/com/donghd/notend/domain/AbstractAuditingEntity.java
+++ server/src/main/java/com/donghd/notend/domain/AbstractAuditingEntity.java
@@ -28,7 +28,7 @@ public abstract class AbstractAuditingEntity implements Serializable {
     @CreatedBy
     @Column(name = "created_by", nullable = false, length = 50, updatable = false)
     @JsonIgnore
-    private String createdBy;
+    private String createdBy = "Sys";
 
     @CreatedDate
     @Column(name = "created_date", nullable = false, updatable = false)
