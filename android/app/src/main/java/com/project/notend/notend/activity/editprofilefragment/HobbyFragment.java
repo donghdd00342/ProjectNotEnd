3198435b125a32c5fa2406b8001cffc0c4e9dcf7
thangnal <thangna@nal.vn>
2018-09-17 17:10:59 +0700

    change_password

---
+++ android/app/src/main/java/com/project/notend/notend/activity/editprofilefragment/HobbyFragment.java
@@ -0,0 +1,44 @@
+package com.project.notend.notend.activity.editprofilefragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.project.notend.notend.R;
+import com.project.notend.notend.activity.ChangePasswordActivity;
+
+public class HobbyFragment extends Fragment{
+    Context context;
+
+//    @BindView(R.id.btn_changepassview)
+//    Button _changePassButton;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_yourself, container, false);
+        context = rootView.getContext();
+        Button _changePassButton = (Button) rootView.findViewById(R.id.btn_changepassview);
+        _changePassButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent i = new Intent(context, ChangePasswordActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return rootView;
+    }
+}
